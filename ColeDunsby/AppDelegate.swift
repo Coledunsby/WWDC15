ffc3d579062b7395aa9fcb24a4c21690188723fc
Cole Dunsby <coledunsby@gmail.com>
2015-04-22 22:08:19 -0400

    Removed Fabric and Crashlytics frameworks to prepare for submission.

--- ColeDunsby/AppDelegate.swift
+++ ColeDunsby/AppDelegate.swift
@@ -7,8 +7,6 @@
 //
 
 import UIKit
-import Fabric
-import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        Fabric.with([Crashlytics()])
-        
+
         UIApplication.sharedApplication().statusBarStyle = .LightContent
         
         UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: UIFont(name: "AvenirNext-Medium", size: 18)!]
